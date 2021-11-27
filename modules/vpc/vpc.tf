8b21ba64da48efc9696df4fba5ec29ca7bafe512
Bryan B <bbartik78@gmail.com>
2021-11-27 08:54:42 -0800

    update

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -10,7 +10,7 @@ resource "aws_subnet" "public" {
   count                   = var.number_of_subnets
   vpc_id                  = aws_vpc.this.id
   cidr_block              = cidrsubnet(var.cidr_block, 4, count.index)
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  availability_zone       = data.aws_availability_zones.this.names[count.index]
   map_public_ip_on_launch = true
 
   tags = {
